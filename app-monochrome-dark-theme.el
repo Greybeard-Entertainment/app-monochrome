982dd54e9621f19692ddb03a2feb68eef2229902
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2023-09-26 16:38:16 +0400

    [feature]: Slight improvements to the theme
    Signed-off-by: Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>

--- app-monochrome-dark-theme.el
+++ app-monochrome-dark-theme.el
@@ -1,4 +1,4 @@
-;;; app-monochrome-dark-theme.el --- a low contrast monochromatic theme that emphasises shapes rather than colours.
+;;; app-monochrome-dark-theme.el --- a low contrast monochromatic theme that emphasises shapes rather than colours.  -*- lexical-binding: t; -*-
 
 ;; Author: Aleksandr Petrosyan <appetrosyan3@gmail.com>
 ;; URL: https://github.com/Greybeard-Entertainment/app-monochrome
@@ -62,20 +62,38 @@
 
 (custom-theme-set-faces
  'app-monochrome-dark
- '(counsel-key-binding ((t (:foreground "peru"))))
- '(font-lock-keyword-face ((t (:weight bold :family "Ubuntu Mono Ligaturized"))))
- '(font-lock-negation-char-face ((t (:weight bold))))
- '(font-lock-doc-face ((t (:slant italic :inherit font-lock-string-face))))
- '(font-lock-string-face ((t (:foreground "grey62" :family "IBM Plex Mono"))))
- '(flyspell-incorrect ((t (:background "light gray" :foreground "red4"))))
- '(font-lock-constant-face ((t (:foreground "white" :weight bold :family "Victor Mono"))))
- '(font-lock-variable-name-face ((t (:foreground "white" :weight thin))))
+ ;; Basic faces
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "grey24" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 98 :width normal :foundry "CTDB" :family "Fira Code"))))
+ '(variable-pitch ((t (:family "IBM Plex Serif"))))
+
+ '(highlight ((t (:background "#440000"))))
+ '(error ((t (:foreground "Red" :weight bold))))
+ '(warning ((t (:foreground "red4"))))
+ '(success ((t (:foreground "white" :weight bold))))
+ '(bookmark-face ((t (:background "Black" :foreground "Gold"))))
+ '(isearch (t (:inherit link)))
+ '(custom-link ((t (:inherit link :box (:line-width (2 . 2) :color "grey75" :style released-button)))))
+ '(link ((t (:underline t :foreground "#5cacac"))))
+ '(line-number ((t (:inherit shadow :family "Victor Mono"))))
+
  '(font-lock-type-face ((t (:weight bold :family "Victor Mono"))))
  '(font-lock-builtin-face ((t (:family "Linux Libertine Mono" :background "grey16"))))
- '(lsp-face-semhl-function ((t (:inherit (font-lock-function-name-face)))))
  '(font-lock-function-name-face ((t (:slant italic :family "IBM Plex Mono"))))
+ '(font-lock-keyword-face ((t (:weight bold :family "Ubuntu Mono"))))
+ '(font-lock-constant-face ((t (:foreground "white" :weight bold :inherit font-lock-type-face))))
+ '(font-lock-string-face ((t (:foreground "grey62" :family "IBM Plex Mono"))))
+ '(font-lock-negation-char-face ((t (:weight bold))))
+ '(font-lock-doc-face ((t (:slant italic :inherit font-lock-string-face))))
+ '(font-lock-variable-name-face ((t (:foreground "white" :weight thin))))
+ '(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face)))))
+
+ '(mode-line ((t (:inherit line-number))))
+ '(mode-line-highlight ((t (:family "Linux Libertine Mono" :inherit mode-line :weight bold))))
+ '(mode-line-buffer-id ((t (:inherit mode-line-highlight))))
+
+ '(lsp-face-semhl-function ((t (:inherit (font-lock-function-name-face)))))
+ '(flyspell-incorrect ((t (:background "light gray" :foreground "red4"))))
  '(lsp-face-highlight-textual ((t (:inherit (highlight)))))
- '(highlight ((t (:background "#440000"))))
  '(lsp-face-highlight-write ((t (:weight bold :inherit (highlight)))))
  '(lsp-face-semhl-type ((t (:inherit (font-lock-type-face)))))
  '(lsp-face-semhl-implementation ((t (:weight bold :inherit (font-lock-function-name-face)))))
@@ -94,19 +112,16 @@
  '(lsp-ui-doc-background ((t (:background "gray30"))))
  '(lsp-ui-sideline-global ((t nil)))
  '(lsp-ui-sideline-symbol ((t (:height 0.99 :box (:line-width (1 . -1) :color "grey" :style nil) :foreground "grey"))))
- '(error ((t (:foreground "Red" :weight bold))))
- '(warning ((t (:foreground "peru"))))
  '(flycheck-fringe-warning ((t (:inherit (warning)))))
  '(flycheck-info ((t (:inherit success :underline t))))
- '(success ((t (:foreground "peru" :weight bold))))
  '(doom-modeline-lsp-success ((t (:inherit nil :weight bold))))
  '(lsp-ui-peek-list ((((background light)) (:background "light gray")) (t (:background "#181818"))))
  '(lsp-ui-peek-line-number ((t (:foreground "grey25"))))
- '(bookmark-face ((t (:background "Black" :foreground "Gold"))))
+
  '(ivy-match-required-face ((t (:foreground "red" :inherit (minibuffer-prompt)))))
  '(ivy-virtual ((t (:inherit (font-lock-builtin-face)))))
  '(ivy-action ((t (:inherit (font-lock-builtin-face)))))
- '(isearch ((((class color) (min-colors 88) (background light)) (:foreground "lightskyblue1" :background "magenta3")) (((class color) (min-colors 88) (background dark)) (:foreground "brown4" :background "palevioletred2")) (((class color) (min-colors 16)) (:foreground "cyan1" :background "magenta4")) (((class color) (min-colors 8)) (:foreground "cyan1" :background "magenta4")) (t (:inverse-video t))))
+
  '(org-list-dt ((t (:bold t))))
  '(compilation-line-number ((t (:inherit (font-lock-keyword-face)))))
  '(doom-modeline-buffer-file ((t (:inherit (mode-line-buffer-id bold)))))
@@ -117,26 +132,26 @@
  '(sp-show-pair-match-content-face ((t nil)))
  '(show-paren-match ((t (:background "red4"))))
  '(show-paren-mismatch ((t (:inherit show-paren-match :inverse-video t))))
- '(variable-pitch ((t (:family "IBM Plex Serif"))))
+
  '(magit-filename ((t (:weight normal))))
  '(magit-keyword-squash ((t (:inherit (font-lock-warning-face)))))
  '(magit-blame-name ((t nil)))
  '(magit-diff-base-highlight ((((class color) (background light)) (:foreground "#aaaa11" :background "#eeeebb" :extend t)) (((class color) (background dark)) (:foreground "#eeeebb" :background "#666622" :extend t))))
  '(magit-keyword ((t (:inherit (font-lock-string-face)))))
- '(magit-tag ((t (:foreground "grey77"))))
+ '(magit-tag ((t (:inherit font-lock-constant-face))))
  '(magit-branch-upstream ((t (:slant italic))))
  '(magit-branch-remote ((t (:foreground "white" :weight bold :height 1.25 :family "IBM Plex Serif"))))
  '(magit-branch-local ((t (:weight bold :height 1.25 :family "IBM Plex Serif"))))
  '(magit-branch-current ((t (:box (:line-width (1 . 1) :color nil :style nil) :inherit (magit-branch-local)))))
  '(magit-section-heading-selection ((t (:extend t :weight bold :height 1.25))))
  '(magit-section-secondary-heading ((t (:weight bold :extend t))))
- '(magit-section-heading ((t (:extend t :weight bold :height 2.0 :family "FoglihtenPCS"))))
+ '(magit-section-heading ((t (:extend t :weight bold :height 2.0 :inherit font-lock-constant-face))))
  '(magit-section-highlight ((((class color) (background light)) (:background "grey95" :extend t)) (((class color) (background dark)) (:background "grey20" :extend t))))
  '(magit-section-child-count ((t nil)))
  '(magit-diff-file-heading ((t (:extend t :weight bold :family "IBM Plex Serif"))))
- '(custom-link ((t (:inherit link :box (:line-width (2 . 2) :color "grey75" :style released-button)))))
+
  '(org-default ((t (:inherit (default)))))
- '(link ((t (:underline t :foreground "#5cacac"))))
+
  '(tree-sitter-hl-face:constructor ((t (:inherit tree-sitter-hl-face:constant))))
  '(tree-sitter-hl-face:property.definition ((t (:inherit (tree-sitter-hl-face:variable.parameter)))))
  '(tree-sitter-hl-face:number ((t (:inherit (tree-sitter-hl-face:constant)))))
@@ -166,18 +181,51 @@
  '(tree-sitter-hl-face:type.argument ((t (:inherit (tree-sitter-hl-face:type)))))
  '(tree-sitter-hl-face:property ((t (:inherit nil :slant italic))))
  '(tree-sitter-hl-face:attribute ((t (:inherit nil))))
- '(font-lock-preprocessor-face ((t (:inherit (font-lock-builtin-face)))))
- '(markdown-header-face-1 ((t (:height 1.5 :inherit markdown-header-face))))
- '(markdown-header-face ((t (:weight bold :inherit magit-section-heading))))
- '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.25))))
- '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.0))))
- '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 0.75))))
+
+
+ '(markdown-header-face ((t (:weight bold :inherit magit-section-heading :height 1.5))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 0.85))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 0.75))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 0.65))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 0.55))))
+ '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 0.55))))
+ '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 0.55))))
+
+ '(org-level-1 ((t (:inherit markdown-header-face))))
+ '(org-level-2 ((t (:inherit markdown-header-face-1))))
+ '(org-level-3 ((t (:inherit markdown-header-face-2))))
+ '(org-level-4 ((t (:inherit markdown-header-face-3))))
+ '(org-level-5 ((t (:inherit markdown-header-face-4))))
+ '(org-level-6 ((t (:inherit markdown-header-face-4 :height 0.85))))
+ '(org-level-7 ((t (:inherit markdown-header-face-4 :height 0.75))))
+ '(org-level-8 ((t (:inherit markdown-header-face-4 :height 0.65))))
+ '(org-code ((t (:family "Victor Mono" :foreground "grey75" :background "grey10"))))
+ '(org-verbatim ((t (:inherit org-code))))
+ '(org-done ((t (:inherit org-headline :foreground "white" :background "#048" :box t))))
+ '(org-todo ((t (:foreground "white" :background "#800" :box t))))
+ '(org-headline-todo ((t (:inherit org-headline))))
+ '(org-headline-done ((t (:inherit org-headline))))
+
+ '(org-modern-done ((t (:inherit org-done :height 2.0))))
+ '(org-modern-todo ((t (:inherit org-todo :height 2.0))))
+
+ '(rainbow-delimiters-depth-1-face ((t (:inherit rainbow-delimiters-base-face))))
+ '(rainbow-delimiters-depth-2-face ((t (:weight bold :inherit rainbow-delimiters-base-face))))
+ '(rainbow-delimiters-depth-3-face ((t (:inherit rainbow-delimiters-base-face :foreground "#fda"))))
+ '(rainbow-delimiters-depth-4-face ((t (:inherit rainbow-delimiters-base-face :foreground "#7fd"))))
+ '(rainbow-delimiters-depth-5-face ((t (:inherit rainbow-delimiters-base-face :foreground "#f0a"))))
+ '(rainbow-delimiters-depth-6-face ((t (:inherit rainbow-delimiters-base-face :foreground "#acf"))))
+ '(rainbow-delimiters-depth-7-face ((t (:inherit rainbow-delimiters-base-face :foreground "#faa"))))
+ '(rainbow-delimiters-depth-8-face ((t (:inherit rainbow-delimiters-base-face :foreground "#0af"))))
+ '(rainbow-delimiters-depth-9-face ((t (:weight bold :inherit rainbow-delimiters-base-face :foreground "#afa"))))
+
+ '(jinx-misspelled ((t (:underline t :inherit warning))))
+
  '(lsp-rust-analyzer-inlay-param-face ((t (:inherit (lsp-rust-analyzer-inlay-face)))))
  '(lsp-rust-analyzer-inlay-type-face ((t (:inherit (lsp-rust-analyzer-inlay-face)))))
  '(lsp-rust-analyzer-inlay-face ((t (:inherit font-lock-comment-face :foreground "black" :background "white"))))
- '(default ((t (:inherit nil :extend nil :stipple nil :background "grey24" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 98 :width normal :foundry "CTDB" :family "Fira Code"))))
- '(solaire-default-face ((t (:inherit default) :background "grey16")))
- )
+
+ '(solaire-default-face ((t (:inherit default) :background "grey16"))))
 
 ;;;###autoload
 (and load-file-name
@@ -189,3 +237,8 @@
 
 (provide-theme 'app-monochrome-dark)
 ;;; app-monochrome-dark-theme.el ends here
+`
+
+;; Local Variables:
+;; jinx-local-words: "Fira Plex el gmail gray"
+;; End:
