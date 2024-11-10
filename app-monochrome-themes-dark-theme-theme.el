c2786e68bb479e345ea312a2179c3712c3b497a1
Aleksandr Petrosyan <ap886@cantab.ac.uk>
2024-11-10 13:22:44 +0400

    [fix]: Legibility of warnings
    Signed-off-by: Aleksandr Petrosyan <ap886@cantab.ac.uk>

--- app-monochrome-themes-dark-theme-theme.el
+++ app-monochrome-themes-dark-theme-theme.el
@@ -64,19 +64,23 @@
  'app-monochrome-themes-dark-theme
  ;; Basic faces
  '(default ((t (:inherit nil :extend nil :stipple nil :background "grey24" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 98 :width normal :foundry "CTDB" :family "Fira Code"))))
+
+ '(dired-directory ((t (:weight bold))))
+ '(dirvish-media-info-property-key ((t nil)))
+
  '(variable-pitch ((t (:family "IBM Plex Serif"))))
  '(highlight ((t (:background "#bcc" :foreground "black"))))
  '(italic ((t (:slant italic :weight normal :family "IBM Plex Sans"))))
  '(error ((t (:box (:line-width (2 . 2) :color "Red" :style released-button) :weight bold))))
- '(warning ((t (:foreground "red4"))))
+ '(warning ((t (:foreground "gold"))))
  '(success ((t (:foreground "white" :weight bold))))
  '(bookmark-face ((t (:background "Black" :foreground "Gold"))))
  '(isearch ((t (:inherit link))))
  '(custom-link ((t (:inherit link :box (:line-width (2 . 2) :color "grey75" :style released-button)))))
  '(link ((t (:underline t :foreground "#5cacac"))))
- '(line-number ((t (:inherit shadow :family "Victor Mono"))))
+ '(line-number ((t (:inherit shadow :family "VictorMono Nerd Font"))))
 
- '(font-lock-type-face ((t (:weight bold :family "Victor Mono"))))
+ '(font-lock-type-face ((t (:weight bold :family "VictorMono Nerd Font"))))
  '(font-lock-builtin-face ((t (:family "Linux Libertine Mono" :background "grey16"))))
  '(font-lock-function-name-face ((t (:slant italic :family "IBM Plex Mono"))))
  '(font-lock-keyword-face ((t (:weight bold :family "Ubuntu Mono"))))
@@ -115,6 +119,8 @@
  '(lsp-ui-peek-list ((((background light)) (:background "light gray")) (t (:background "#181818"))))
  '(lsp-ui-peek-line-number ((t (:foreground "grey25"))))
 
+ '(flymake-error ((t (:underline (:color "Red1" :style dashes :position nil)))))
+ 
  '(flycheck-fringe-warning ((t (:inherit (warning)))))
  '(flycheck-info ((t (:inherit success :underline t))))
 
@@ -205,6 +211,7 @@
  '(markdown-header-face-4 ((t (:inherit markdown-header-face-3 :height 0.8))))
  '(markdown-header-face-5 ((t (:inherit markdown-header-face-4 :height 0.8))))
  '(markdown-header-face-6 ((t (:inherit markdown-header-face-4 :height 0.8))))
+ '(markdown-inline-code-face ((t (:inherit org-code))))
 
  '(org-level-1 ((t (:inherit markdown-header-face))))
  '(org-level-2 ((t (:inherit markdown-header-face-1))))
@@ -215,7 +222,7 @@
  '(org-level-7 ((t (:inherit markdown-header-face-4 :height 0.75))))
  '(org-level-8 ((t (:inherit markdown-header-face-4 :height 0.65))))
 
- '(org-code ((t (:family "Victor Mono" :foreground "grey75" :background "grey10"))))
+ '(org-code ((t (:family "VictorMono Nerd Font" :foreground "grey75" :background "grey10"))))
  '(org-verbatim ((t (:inherit org-code :weight light))))
  '(org-done ((t (:inherit org-headline :foreground "white" :background "#048" :box t))))
  '(org-todo ((t (:foreground "white" :background "#800" :box t))))
@@ -247,6 +254,7 @@
  '(lsp-rust-analyzer-inlay-param-face ((t (:inherit (lsp-rust-analyzer-inlay-face)))))
  '(lsp-rust-analyzer-inlay-type-face ((t (:inherit (lsp-rust-analyzer-inlay-face)))))
  '(lsp-rust-analyzer-inlay-face ((t (:inherit font-lock-comment-face :foreground "black" :background "white"))))
+ '(lsp-ui-doc-background ((t (:background "grey10"))))
 
  '(rust-unsafe ((t (:weight bold))))
 
